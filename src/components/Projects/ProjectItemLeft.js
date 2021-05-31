87102d5a8b6bb56bacf4fb4999bd58b763fa8aeb
Sara Lotfi <lotfi.sara@gmail.com>
2021-05-31 14:48:52 +0200

    add fontawesome icons

--- src/components/Projects/ProjectItemLeft.js
+++ src/components/Projects/ProjectItemLeft.js
@@ -1,6 +1,10 @@
 import './ProjectItemLeft.css';
 import pic from "../../images/meta-img.png"
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons'
+import { faExternalLinkSquareAlt } from '@fortawesome/free-solid-svg-icons';
+
 
 const ProjectItemLeft = () => {
 
@@ -14,6 +18,8 @@ const ProjectItemLeft = () => {
         <p className="text-orange">Team Project</p>
         <h2 className="text-700">Wellsy</h2>
         <p className="project-text mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occ</p>
+        <a href="https://github.com/saralotfi/float_boat" target="_blank" rel="noopener noreferrer"><p><FontAwesomeIcon icon={faGithubSquare} /></p></a>
+        <a href="https://floatifyapp.herokuapp.com" target="_blank" rel="noopener noreferrer"><p><FontAwesomeIcon icon={faExternalLinkSquareAlt} /></p></a>
       </div>
       <div className="project-img">
         <div className="image" style={style}></div>
