c979e6b51b46eafe6f52a955eaff451cc5990a66
Sara Lotfi <lotfi.sara@gmail.com>
2021-06-01 15:04:45 +0200

    fix list overflow in about for small device

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -5,9 +5,9 @@ const Intro = () => {
     <div id="intro">
       <p className="text-orange text-300 text-lg">Hi, my name is</p>
       <h1>Sara Lotfi</h1>
-      <p className="text-md text-300 mt-3">I’m a developer and illustrator based in Uppsala, Sweden. I'm currently looking to join a team as a front end developer. Most recently I graduated from Le Wagon, a full-time web development bootcamp. Here's a few technologies I've worked with: </p>
-      <div className="d-flex text-20px mt-3">
-        <div className="ml-5">
+      <p className="text-md text-300 mt-3 mb-4">I’m a developer and illustrator based in Uppsala, Sweden. I'm currently looking to join a team as a front end developer. Most recently I graduated from Le Wagon, a full-time web development bootcamp. Here's a few technologies I've worked with: </p>
+      <div className="tech-stack text-20px">
+        <div>
           <ul className="text-orange">
             <li><span>React</span></li>
             <li><span>JavaScript (ES6+)</span></li>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,42 +1,11 @@
-// import './Navbar.css';
-
-// const Navbar = () => {
-//   return (
-//     <nav className="navbar navbar-expand-sm navbar-light sticky-top">
-//       <a className="navbar-brand" href="#">👀</a>
-
-//       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-//         <span className="navbar-toggler-icon"></span>
-//       </button>
-
-//       <div className="collapse navbar-collapse" id="navbarNav">
-//         <ul className="navbar-nav ms-auto">
-//           <li className="nav-item active">
-//             <a className="nav-link" href="#">About <span className="sr-only">(current)</span></a>
-//           </li>
-//           <li className="nav-item">
-//             <a className="nav-link" href="#">Projects</a>
-//           </li>
-//           <li className="nav-item">
-//             <a className="nav-link" href="#">Contact</a>
-//           </li>
-//         </ul>
-//       </div>
-//     </nav>
-//   )
-// }
-
-// export default Navbar;
-
 import './Navbar.css';
 
-
-import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 
 const NavbarEx = () => {
   return (
     <Navbar collapseOnSelect expand="lg" sticky="top" className="navbar">
-      <Navbar.Brand className="navbar-brand" href="#home">👀</Navbar.Brand>
+      <Navbar.Brand className="navbar-brand" href="#top">👀</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="ml-auto">
