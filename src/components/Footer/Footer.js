19dae6dc85c8c563ba52531a0b3e7988ef2b7b27
Sara Lotfi <lotfi.sara@gmail.com>
2021-06-02 20:56:44 +0200

    updated  gh link in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="social-links">
-        <a href="https://github.com/saralotfi/float_boat" target="_blank" rel="noopener noreferrer"><p className="text-orange text-md"><FontAwesomeIcon icon={faGithubSquare} /></p></a>
+        <a href="https://github.com/saralotfi" target="_blank" rel="noopener noreferrer"><p className="text-orange text-md"><FontAwesomeIcon icon={faGithubSquare} /></p></a>
         <a href="https://www.linkedin.com/in/sara-lotfi/" target="_blank" rel="noopener noreferrer"><p className="text-orange text-md"><FontAwesomeIcon icon={faLinkedin} /></p></a>
       </div>
       <p className="text-orange mb-5 mt-4">© 2021 Sara Lotfi. All rights reserved.</p>
