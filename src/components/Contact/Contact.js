3be88b5c084d7be2706b61232a184331cc1cfd43
Sara Lotfi <lotfi.sara@gmail.com>
2021-06-22 15:07:16 +0200

    update intro and contact text

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,12 +1,24 @@
-import './Contact.css';
+import "./Contact.css";
 
 const Contact = () => {
   return (
     <div id="contact">
       <h1>Hey there!</h1>
-      <p className="text-md text-300 mt-3">I'm currently looking for opportunities in front-end development. If you would like to get in contact, pop me an e-mail at <a href="mailto: lotfi.sara@gmail.com">lotfi.sara@gmail.com</a> or message me on <a href="https://www.linkedin.com/in/sara-lotfi/" target="_blank" rel="noopener noreferrer">Linked In</a>.</p>
+      <p className="text-md text-300 mt-3">
+        If you would like to get in contact, pop me an e-mail at{" "}
+        <a href="mailto: lotfi.sara@gmail.com">lotfi.sara@gmail.com</a> or
+        message me on{" "}
+        <a
+          href="https://www.linkedin.com/in/sara-lotfi/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Linked In
+        </a>
+        .
+      </p>
     </div>
-  )
-}
+  );
+};
 
 export default Contact;
--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,33 +1,57 @@
-import './Intro.css';
+import "./Intro.css";
 
 const Intro = () => {
   return (
     <div id="intro">
       <p className="text-orange text-300 text-lg">Hi, my name is</p>
       <h1>Sara Lotfi</h1>
-      <p className="text-md text-300 mt-3 mb-4">I’m a web developer and illustrator based in Uppsala, Sweden. I'm currently looking to join an engineering team as a front-end developer. Most recently I graduated from Le Wagon, a full-time web development bootcamp. Here's a few technologies I've worked with: </p>
+      <p className="text-md text-300 mt-3 mb-4">
+        I’m a full-stack developer and illustrator based in Uppsala, Sweden.
+        Most recently I graduated from Le Wagon, a full-time web development
+        bootcamp. Here's a few technologies I've worked with:{" "}
+      </p>
       <div className="tech-stack text-20px">
         <div>
           <ul className="text-orange">
-            <li><span>React</span></li>
-            <li><span>JavaScript (ES6+)</span></li>
-            <li><span>Ruby on Rails</span></li>
-            <li><span>HTML/CSS/Bootstrap</span></li>
-            <li><span>Git and GitHub</span></li>
+            <li>
+              <span>React</span>
+            </li>
+            <li>
+              <span>JavaScript (ES6+)</span>
+            </li>
+            <li>
+              <span>Ruby on Rails</span>
+            </li>
+            <li>
+              <span>HTML/CSS/Bootstrap</span>
+            </li>
+            <li>
+              <span>Git and GitHub</span>
+            </li>
           </ul>
         </div>
         <div>
           <ul className="text-orange">
-            <li><span>Heroku</span></li>
-            <li><span>JSON</span></li>
-            <li><span>APIs</span></li>
-            <li><span>SQL</span></li>
-            <li><span>Relational databases</span></li>
+            <li>
+              <span>Heroku</span>
+            </li>
+            <li>
+              <span>JSON</span>
+            </li>
+            <li>
+              <span>APIs</span>
+            </li>
+            <li>
+              <span>SQL</span>
+            </li>
+            <li>
+              <span>Relational databases</span>
+            </li>
           </ul>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Intro;
