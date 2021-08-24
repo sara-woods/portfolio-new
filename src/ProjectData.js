27dce0c534f2144534ecd6336f56c98ef0be88c5
Sara Lotfi <lotfi.sara@gmail.com>
2021-08-24 17:43:22 +0200

    update wellsy url

--- src/ProjectData.js
+++ src/ProjectData.js
@@ -3,46 +3,77 @@ export const ProjectData = [
     id: 1,
     type: "Solo project",
     name: "What movie?",
-    description: "Movie generator built in React with a Rails back-end API to fetch movies. Users can generate a random movie or filter the result based on release year, rating and genres.",
-    technologies: ["React", "Ruby on Rails", "JSON", "API", "Heroku", "HTML/SCSS"],
+    description:
+      "Movie generator built in React with a Rails back-end API to fetch movies. Users can generate a random movie or filter the result based on release year, rating and genres.",
+    technologies: [
+      "React",
+      "Ruby on Rails",
+      "JSON",
+      "API",
+      "Heroku",
+      "HTML/SCSS",
+    ],
     repo: "https://github.com/saralotfi/rails-find-me-a-movie",
-    web: "https://whatmovieapp.herokuapp.com/"
+    web: "https://whatmovieapp.herokuapp.com/",
   },
   {
     id: 2,
     type: "Team project",
     name: "Wellsy",
-    description: "A platform where users can create or book workout sessions and participate via zoom from their own home. Includes search, authorization, image-hosting and payments via Stripe. Built in 10 days.",
-    technologies: ["Ruby on Rails", "JavaScript ES6", "Stripe", "Heroku", "HTML/SCSS", "PostgreSQL"],
+    description:
+      "A platform where users can create or book workout sessions and participate via zoom from their own home. Includes search, authorization, image-hosting and payments via Stripe. Built in 10 days.",
+    technologies: [
+      "Ruby on Rails",
+      "JavaScript ES6",
+      "Stripe",
+      "Heroku",
+      "HTML/SCSS",
+      "PostgreSQL",
+    ],
     repo: "https://github.com/saralotfi/wellsy",
-    web: "http://wellsy.live/"
+    web: "https://wellsy.herokuapp.com/",
   },
   {
     id: 3,
     type: "Team project",
     name: "Packathon",
-    description: "Winner of Hackathon - Le Wagon, an order packing-helper app for online retailers using Ruby on Rails and Three.js. (The site breaks at the moment due to the external packing algorithm API currently being down.)",
-    technologies: ["Ruby on Rails", "Three.js", "JavaScript ES6", "Heroku", "HTML/SCSS"],
+    description:
+      "Winner of Hackathon - Le Wagon, an order packing-helper app for online retailers using Ruby on Rails and Three.js. (The site breaks at the moment due to the external packing algorithm API currently being down.)",
+    technologies: [
+      "Ruby on Rails",
+      "Three.js",
+      "JavaScript ES6",
+      "Heroku",
+      "HTML/SCSS",
+    ],
     repo: "https://github.com/thomas-kenny/veeqo",
-    web: "https://packathon.herokuapp.com/"
+    web: "https://packathon.herokuapp.com/",
   },
   {
     id: 4,
     type: "Solo project",
     name: "Catsweeper",
-    description: "Based on the game Minesweeper and built in JavaScript. I recreated the game mechanics as close to the classic game as possible. The design includes my own illustrations.",
+    description:
+      "Based on the game Minesweeper and built in JavaScript. I recreated the game mechanics as close to the classic game as possible. The design includes my own illustrations.",
     technologies: ["JavaScript ES6", "HTML", "CSS"],
     repo: "https://github.com/saralotfi/catsweeper",
-    web: "https://saralotfi.github.io/catsweeper/"
-
+    web: "https://saralotfi.github.io/catsweeper/",
   },
   {
     id: 5,
     type: "Team project",
     name: "Floatify",
-    description: "An Airbnb like platform built in Ruby on Rails featuring search, geocoding and authorization. Users can create or rent boats as well as manage and review their bookings. Built in 5 days.",
-    technologies: ["Ruby on Rails", "JavaScript ES6", "Mapbox", "Heroku", "HTML/SCSS", "PostgreSQL"],
+    description:
+      "An Airbnb like platform built in Ruby on Rails featuring search, geocoding and authorization. Users can create or rent boats as well as manage and review their bookings. Built in 5 days.",
+    technologies: [
+      "Ruby on Rails",
+      "JavaScript ES6",
+      "Mapbox",
+      "Heroku",
+      "HTML/SCSS",
+      "PostgreSQL",
+    ],
     repo: "https://github.com/saralotfi/float_boat",
-    web: "https://floatifyapp.herokuapp.com/"
-  }
-]
+    web: "https://floatifyapp.herokuapp.com/",
+  },
+];
