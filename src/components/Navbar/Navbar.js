596d7cfcc65296faabe5efc9700445798455d37e
Sara Lotfi <lotfi.sara@gmail.com>
2021-10-04 15:21:36 +0200

    add tab-system for projects

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,29 @@
-import './Navbar.css';
+import "./Navbar.css";
 
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav } from "react-bootstrap";
 
 const NavbarEx = () => {
   return (
     <Navbar collapseOnSelect expand="lg" sticky="top" className="navbar">
-      <Navbar.Brand className="navbar-brand" href="#top">👀</Navbar.Brand>
+      <Navbar.Brand className="navbar-brand" href="#top">
+        👀
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link className="navbar-link" href="#projects">Projects</Nav.Link>
-          <Nav.Link className="navbar-link" href="#top">About</Nav.Link>
-          <Nav.Link className="navbar-link" href="#contact">Contact</Nav.Link>
+          <Nav.Link className="navbar-link" href="#projects-anchor">
+            Projects
+          </Nav.Link>
+          <Nav.Link className="navbar-link" href="#top">
+            About
+          </Nav.Link>
+          <Nav.Link className="navbar-link" href="#contact">
+            Contact
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
-  )
-}
+  );
+};
 
 export default NavbarEx;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,19 +1,51 @@
+import { useState } from "react";
 import "./Projects.css";
 import WebDevProjects from "./WebDevProjects";
 import Illustrations from "./Illustrations";
 import GraphicDesign from "./GraphicDesign";
 
 const Projects = (props) => {
-  console.log(props.projectData);
+  const [page, setPage] = useState("1");
+
+  const handlePageChange = (event) => {
+    setPage(event.target.dataset.page);
+  };
+
   return (
     <div id="projects">
-      <h1 className="mb-3">Projects</h1>
-      <WebDevProjects
-        projectData={props.projectData}
-        projectImages={props.projectImages}
-      />
-      <Illustrations />
-      <GraphicDesign />
+      <div id="projects-anchor"></div>
+      <h1 className="mb-5">Projects</h1>
+      <div className="tabs">
+        <p
+          data-page="1"
+          className={`tab ${page === "1" ? "underline" : ""}`}
+          onClick={handlePageChange}
+        >
+          Web Development
+        </p>
+        <p
+          data-page="2"
+          className={`tab ${page === "2" ? "underline" : ""}`}
+          onClick={handlePageChange}
+        >
+          Illustrations
+        </p>
+        <p
+          data-page="3"
+          className={`tab ${page === "3" ? "underline" : ""}`}
+          onClick={handlePageChange}
+        >
+          Logos/Web Design
+        </p>
+      </div>
+      {page === "1" && (
+        <WebDevProjects
+          projectData={props.projectData}
+          projectImages={props.projectImages}
+        />
+      )}
+      {page === "2" && <Illustrations />}
+      {page === "3" && <GraphicDesign />}
     </div>
   );
 };
