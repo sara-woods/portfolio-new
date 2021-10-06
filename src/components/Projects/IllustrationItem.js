d52a961451cac15b748838bc09787125160a1118
Sara Lotfi <lotfi.sara@gmail.com>
2021-10-06 10:48:31 +0200

    prevent drag and right-click on images

--- src/components/Projects/IllustrationItem.js
+++ src/components/Projects/IllustrationItem.js
@@ -1,9 +1,23 @@
 import "./IllustrationItem.css";
 
 const IllustrationItem = (props) => {
+  const handleRightClick = (event) => {
+    event.preventDefault();
+  };
+
+  const handleDrag = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div className="illustration-container">
-      <img className="illustration" src={props.src} alt={props.alt} />
+      <img
+        onContextMenu={handleRightClick}
+        onDragStart={handleDrag}
+        className="illustration"
+        src={props.src}
+        alt={props.alt}
+      />
     </div>
   );
 };
