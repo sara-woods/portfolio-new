e1e0bdf9172121b6ea58c0347267e6e3c18f3866
Sara Woods <hellosarawoods@gmail.com>
2022-11-26 14:42:42 +0100

    Remove pinging project urls after moving from heroku to fly.io

--- src/App.js
+++ src/App.js
@@ -8,10 +8,6 @@ import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 
-import { en } from "./translations/en";
-
-const projectData = en.translation.projectData;
-
 const App = () => {
   const { i18n } = useTranslation();
 
@@ -22,27 +18,6 @@ const App = () => {
       document.documentElement.lang = "en";
     }
   }, [i18n.language]);
-
-  useEffect(() => {
-    const urls = [];
-
-    for (const property in projectData) {
-      urls.push(projectData[property].web);
-    }
-
-    // ping heroku urls
-    const ping = (url) => {
-      fetch(url, { mode: "no-cors" })
-        .then((response) => {
-          //
-        })
-        .catch((error) => {
-          //
-        });
-    };
-
-    urls.forEach((url) => ping(url));
-  }, []);
 
   return (
     <>
