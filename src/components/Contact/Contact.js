02d59b2eb3bd7bfbb260102a5a6293361494e084
Sara Woods <hellosarawoods@gmail.com>
2024-02-08 11:30:04 +0100

    Update footer and fix contact section

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
   return (
     <WrapperBackground backgroundColor="#f2f2f2" id="wrapper-contact">
       <div className="main-container" id="scroll-contact">
-        <h1>Let's chat</h1>
+        <h1>Let's connect</h1>
         <a href="mailto: hello@sarawoodsdesign.com"><p>hellosarawoods@gmail.com</p></a>
       </div>
     </WrapperBackground>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,35 +1,36 @@
 import "./Footer.css";
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import WrapperBackground from "../UI/WrapperBackground";
 
 const Footer = () => {
   return (
-    <div className="footer">
+    <WrapperBackground id="footer-wrapper" backgroundColor="#f2f2f2">
+
+    <div className="footer main-container">
       <div className="social-links">
-        <a
-          href="https://github.com/sara-woods"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="tab-link"
-        >
-          <p className="text-orange text-md">
-            <FontAwesomeIcon icon={faGithubSquare} />
-          </p>
-        </a>
         <a
           href="https://www.linkedin.com/in/hellosarawoods/"
           target="_blank"
           rel="noopener noreferrer"
           className="tab-link"
         >
-          <p className="text-orange text-md">
-            <FontAwesomeIcon icon={faLinkedin} />
+          <p className="">
+            LinkedIn
+          </p>
+        </a>
+        <a
+          href="https://github.com/sara-woods"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="tab-link"
+          >
+          <p className="">
+            GitHub
           </p>
         </a>
       </div>
-      <p className="text-orange mb-5 mt-4">© 2024 Sara Woods</p>
+      <p className="copyright">© 2024 Sara Woods</p>
     </div>
+          </WrapperBackground>
   );
 };
 
