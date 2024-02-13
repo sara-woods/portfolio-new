9d594e9a5c25705675d6bc42aeb36b4b2613d3b1
Sara Woods <hellosarawoods@gmail.com>
2024-02-13 18:10:30 +0100

    Add images and fonts

--- src/components/UI/ColorBackground.js
+++ src/components/UI/ColorBackground.js
@@ -1,11 +1,12 @@
 import "./ColorBackground.css";
 
 const ColorBackground = (props) => {
+  console.log(props.backgroundColor);
   return (
     <div
       id={props.id}
       className={`color-background ${props.className}`}
-      style={{ backgroundColor: props.backgroundColor }}
+      style={{ background: props.backgroundColor }}
     >
       {props.children}
     </div>
