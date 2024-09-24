9e02dcd93c02fdbd1a803da48a369b1312264325
Sara Woods <hellosarawoods@gmail.com>
2024-09-24 11:40:41 +0200

    Add graphic designs captions

--- src/components/Projects/GraphicDesignPage/GraphicDesign.js
+++ src/components/Projects/GraphicDesignPage/GraphicDesign.js
@@ -39,7 +39,7 @@ const GraphicDesign = () => {
           <IllustrationItem
             src={beachImage}
             alt="Beachy company logo design"
-            description="Logo design for company selling beach totes. The logo mark was inspired by the sun and a bay seen from above."
+            description="Logo design for a company selling beach totes. The logo mark was inspired by the sun and a bay seen from above."
           />
           <IllustrationItem
             src={musikkolloImage}
@@ -49,13 +49,19 @@ const GraphicDesign = () => {
           <IllustrationItem
             src={posterMusicImage}
             alt="Poster design for a music fest"
-            description="Poster design for a local music fest with a hand lettered main title/logo."
+            description="Poster design for a local music fest with a hand lettered main title/logo.
+Prerequisites for making the design was to use the pre-existent background graphic and to create the poster in Canva."
           />
           <IllustrationItem
             src={photographyImage}
             alt="Logo design for a photographer business."
+            description="Logo design for a photography studio."
           />
-          <IllustrationItem src={symmetryImage} alt="Yoga studio logo design" />
+          <IllustrationItem
+            src={symmetryImage}
+            alt="Yoga studio logo design"
+            description="Logo design for a yoga studio."
+          />
           {/* <IllustrationItem
             src={bagImage}
             alt="Tote bag with outdoorsy illustration"
@@ -73,15 +79,17 @@ const GraphicDesign = () => {
           <IllustrationItem
             src={paddleboardsImage}
             alt="Stand up paddle boards logo design"
-            description="Logo design for a stand up paddling (SUP) boards brand."
+            description="Logo design for a stand up paddling (SUP) boards."
           />
           <IllustrationItem
             src={publishingImage}
             alt="Logo design for a publishing company"
+            description="Book publisher logo design."
           />
           <IllustrationItem
             src={rosesImage}
             alt="T-shirt design with roses and text"
+            description="T-shirt design for a fictive brand."
           />
         </div>
       </div>
